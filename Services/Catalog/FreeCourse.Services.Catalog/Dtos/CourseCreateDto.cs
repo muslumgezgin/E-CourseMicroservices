85517513325d19513544cdf23bfea1206e406941
muslum <muslum_gezgin@hotmail.com>
2022-03-15 16:44:18 +0100

    create dto objects

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/CourseCreateDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace FreeCourse.Services.Catalog.Dtos
+{
+	public class CourseCreateDto
+	{
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string UserID { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Picture { get; set; }
+
+        public FeatureDto Feature { get; set; }
+
+        public string CategoryId { get; set; }
+
+    }
+}
+
