7b38563f539de560b0ec630042abb7ec384c1b57
muslum <muslum_gezgin@hotmail.com>
2022-03-28 14:39:51 +0200

    IBasket interface and Redis setting class

---
+++ Services/Basket/FreeCourse.Services.Basket/Settings/RedisSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace FreeCourse.Services.Basket.Settings
+{
+	public class RedisSettings
+	{
+        public string Host { get; set; }
+
+        public string Port { get; set; }
+    }
+}
+
--- Services/Basket/FreeCourse.Services.Basket/Startup.cs
+++ Services/Basket/FreeCourse.Services.Basket/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FreeCourse.Services.Basket.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -25,7 +26,7 @@ namespace FreeCourse.Services.Basket
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.Configure<RedisSettings>(Configuration.GetSection("RedisSettings"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
