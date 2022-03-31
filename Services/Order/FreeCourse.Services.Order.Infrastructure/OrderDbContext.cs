7a24c3b446f970d1ada37224d9071edd52d3431b
muslum <muslum_gezgin@hotmail.com>
2022-03-31 14:37:03 +0200

    create dtos in the application layer

--- Services/Order/FreeCourse.Services.Order.Infrastructure/OrderDbContext.cs
+++ Services/Order/FreeCourse.Services.Order.Infrastructure/OrderDbContext.cs
@@ -18,8 +18,11 @@ namespace FreeCourse.Services.Order.Infrastructure
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Domain.OrderAggregate.Order>().ToTable("Orders", DEFAULT_SCHEMA);
+
             modelBuilder.Entity<Domain.OrderAggregate.OrderItem>().ToTable("OrderItems", DEFAULT_SCHEMA);
+
             modelBuilder.Entity<Domain.OrderAggregate.OrderItem>().Property(x => x.Price).HasColumnType("decimal(18,2)");
+
             modelBuilder.Entity<Domain.OrderAggregate.Order>().OwnsOne(e => e.Address).WithOwner();
 
             base.OnModelCreating(modelBuilder);
