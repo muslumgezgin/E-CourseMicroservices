f0d1be2ad6a331cd0325a9f0f6a217261d53315a
muslum <muslum_gezgin@hotmail.com>
2022-04-03 17:04:51 +0200

    add fakePayment controllers

---
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FreeCourse.Services.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentsController : CustomBaseController
+    {
+
+        [HttpPost]
+        public IActionResult RecivePayment()
+        {
+            return CreateActionResultInstance(Response<NoContent>.Success(200));
+        }
+      
+    }
+}
+
