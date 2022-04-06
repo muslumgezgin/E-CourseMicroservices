88bbaad3eff0cb76c1762beadd6ae5d45161105d
muslum <muslum_gezgin@hotmail.com>
2022-04-06 15:39:26 +0200

    implement IIdentity service and Identity model

---
+++ Frontends/FreeCourse.Web/Models/SignInInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models
+{
+	public class SigninInput
+	{
+        [Display(Name ="Your email address")]
+        public string Email { get; set; }
+
+        [Display(Name = "Your password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool IsRemember { get; set; }
+
+    }
+}
+
---
+++ Frontends/FreeCourse.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using FreeCourse.Shared.Dtos;
+using IdentityModel.Client;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+	public interface IIdentityService
+	{
+
+		Task<Response<bool>> SignIn();
+
+		Task<TokenResponse> GetAccesTokenByRefreshToken();
+
+		Task RevokeRefreshToken();
+        
+	}
+}
+
