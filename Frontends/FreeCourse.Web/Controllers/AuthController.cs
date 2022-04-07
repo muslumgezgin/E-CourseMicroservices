ef6f681c042b89efd71bdbe69012e5c9e19127bc
muslum <muslum_gezgin@hotmail.com>
2022-04-07 11:41:28 +0200

    add auth controller

---
+++ Frontends/FreeCourse.Web/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreeCourse.Web.Models;
+using FreeCourse.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FreeCourse.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IIdentityService _identityService;
+
+        public AuthController(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+
+        // GET: /<controller>/
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async IActionResult SignIn(SigninInput signinInput)
+        {
+            if(ModelState.IsValid)
+            {
+                return View();
+
+            }
+
+            var response = await _identityService.SignIn(signinInput);
+
+            if(!response.IsSuccesful)
+            {
+                response.Errors.ForEach(s =>
+                {
+                    ModelState.AddModelError(String.Empty, s);
+                });
+
+
+                return View();
+            }
+
+            return RedirectToAction(nameof(Index), "Home");
+
+
+        }
+    }
+}
+
--- Frontends/FreeCourse.Web/Models/SignInInput.cs
+++ Frontends/FreeCourse.Web/Models/SignInInput.cs
@@ -5,9 +5,11 @@ namespace FreeCourse.Web.Models
 {
 	public class SigninInput
 	{
+        [Required]
         [Display(Name ="Your email address")]
         public string Email { get; set; }
 
+        [Required]
         [Display(Name = "Your password")]
         public string Password { get; set; }
 
