46d14797dbcebe787aaadb5b66d34e58ddcbbb19
muslum <muslum_gezgin@hotmail.com>
2022-04-16 20:00:20 +0200

    update-delete a photo/course controller

--- Frontends/FreeCourse.Web/Models/Catalog/CourseUpdateInput.cs
+++ Frontends/FreeCourse.Web/Models/Catalog/CourseUpdateInput.cs
@@ -1,22 +1,34 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
 namespace FreeCourse.Web.Models.Catalog
 {
 	public class CourseUpdateInput
 	{
         public string Id { get; set; }
+
+        [Display(Name = "Course Name")]
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
         public string UserID { get; set; }
 
+        [Required]
         public decimal Price { get; set; }
 
         public FeatureViewModel Feature { get; set; }
 
+        [Required]
         public string CategoryId { get; set; }
 
         public string Picture { get; set; }
+
+        [Display(Name = "Course Picture")]
+        public IFormFile PhotoFormFile { get; set; }
     }
 }
 
