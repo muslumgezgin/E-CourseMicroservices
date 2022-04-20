b8ce148b18eb7a796cc6df95ced391c1f62e8bd9
muslum <muslum_gezgin@hotmail.com>
2022-04-20 12:52:49 +0200

    Discount Service method

--- Frontends/FreeCourse.Web/Services/DiscountService.cs
+++ Frontends/FreeCourse.Web/Services/DiscountService.cs
@@ -1,11 +1,35 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using FreeCourse.Shared.Dtos;
+using FreeCourse.Web.Models.Discount;
+using FreeCourse.Web.Services.Interfaces;
+
 namespace FreeCourse.Web.Services
 {
-	public class DiscountService
-	{
-		public DiscountService()
-		{
-		}
-	}
+    public class DiscountService : IDiscountService
+    {
+        private readonly HttpClient _httpClient;
+
+        public DiscountService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<DiscountViewModel> GetDiscount(string discountCode)
+        {
+            var response = await _httpClient.GetAsync($"discount/getbycode/{discountCode}");
+
+            if(!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            var discount = await response.Content.ReadFromJsonAsync<Response<DiscountViewModel>>();
+
+            return discount.Data;
+        }
+    }
 }
 
