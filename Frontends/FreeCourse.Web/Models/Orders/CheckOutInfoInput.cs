ef17e84e92661f18f0e819855aa5e2f4e89ed38d
muslum <muslum_gezgin@hotmail.com>
2022-04-20 17:03:49 +0200

    checkout input info model

---
+++ Frontends/FreeCourse.Web/Models/Orders/CheckOutInfoInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models.Orders
+{
+	public class CheckOutInfoInput
+	{
+		public string Province { get; set; }
+
+		public string District { get; set; }
+
+		public string Street { get; set; }
+
+		public string ZipCode { get; set; }
+
+		public string Line { get; set; }
+
+		[Display(Name ="Card name and surname")]
+		public string CardName { get; set; }
+
+		public string CardNumber { get; set; }
+
+		[Display(Name ="expiration date")]
+		public string Expiration { get; set; }
+
+		[Display(Name ="CVC2/CVV number")]
+		public string CVC { get; set; }
+
+	}
+}
+
