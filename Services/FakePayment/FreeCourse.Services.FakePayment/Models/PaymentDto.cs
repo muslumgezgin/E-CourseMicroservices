20bb75c7f0f885a0a860b8e0a713226f2b5d9e20
muslum <muslum_gezgin@hotmail.com>
2022-04-21 12:39:14 +0200

    Fake payment/ FakePaymentDto

--- Services/FakePayment/FreeCourse.Services.FakePayment/Models/PaymentDto.cs
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Models/PaymentDto.cs
@@ -4,10 +4,16 @@ namespace FreeCourse.Services.FakePayment.Models
 	public class PaymentDto
 	{
 		public string CardName { get; set; }
+
 		public string CardNumber { get; set; }
+
 		public string Expiration { get; set; }
+
 		public string CVC { get; set; }
+
 		public decimal TotalPrice { get; set; }
-	}
+
+        public OrderDto Order { get; set; }
+    }
 }
 
